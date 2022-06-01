6d1dcc3639fed2af0254fb54f7dcec432047098c
Beibars <damskali97@gmail.com>
2022-06-01 22:47:03 +0600

    1

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+import pizzas from './assets/pizzas.json';
+
+import './scss/app.scss';
+
+console.log(pizzas);
+
+function App() {
+  return (
+  <div className="App">
+    <div className="wrapper">
+ <Header/>
+<div className="content">
+  <div className="container">
+    <div className="content__top">
+      <Categories/>
+      <Sort/>
+    </div>
+    <h2 className="content__title">Все пиццы</h2>
+    <div className="content__items">
+    {
+      pizzas.map((obj) => (
+        <PizzaBlock {...obj}/> 
+      ))}
+        
+     
+     
+    </div> 
+  </div>
+</div>
+</div>
+  </div> 
+  );
+} 
+
+export default App;
