e8f3fc9977478e90aa59604689dfefa5f654a486
Beibars <damskali97@gmail.com>
2022-06-12 15:45:44 +0600

    2

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
+
 
 import './scss/app.scss';
 
-console.log(pizzas);
 
 function App() {
   return (
-  <div className="App">
     <div className="wrapper">
  <Header/>
 <div className="content">
-  <div className="container">
-    <div className="content__top">
-      <Categories/>
-      <Sort/>
-    </div>
-    <h2 className="content__title">Все пиццы</h2>
-    <div className="content__items">
-    {
-      pizzas.map((obj) => (
-        <PizzaBlock {...obj}/> 
-      ))}
-        
-     
-     
-    </div> 
+ 
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/cart' element={<Cart/>} />
+      <Route path='*' element={<NotFound/>} />
+    </Routes>
+    
   </div>
 </div>
-</div>
-  </div> 
+  
   );
 } 
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: 'популярности',
+    sortProperty: 'rating',
+  });
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    const order = sortType.sortProperty.includes('-')
+      ? 'asc'
+      : 'desc'; /*если есть мнус то это иначе это*/
+    const sortBy = sortType.sortProperty.replace('-', ''); /*удаляет минус*/
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+
+    fetch(
+      `https://629c98bb3798759975d82d84.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+    )
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, [categoryId, sortType]);
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)} />
+        <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
